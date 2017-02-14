4095f029d9cbd4155364f655d01ef8b22c4de8bb
owintwist <owintwist@free.fr>
2017-02-14 02:34:58 +0100

    0.2 — add ::getMergedResults() + filters

--- IncandescentAPI.class.php
+++ IncandescentAPI.class.php
@@ -2,7 +2,7 @@
 	
 	/***
 	 * 
-	 *	IncandescentAPI PHP Class v0.1
+	 *	IncandescentAPI PHP Class v0.2
 	 *	https://github.com/owintwist/IncandescentAPI-PHP-Class
 	 * 
 	 ***/
@@ -11,25 +11,52 @@
 	const UID = '';			// Your API UID
 	const KEY = '';			// Your API KEY
 	const EXPIRES = 300;	// Default auth expiration delay
+	const FILTERSDIR = '.';	// Filters' dir without ending "/"
 	
 
 	public static function addImage($images,$c=1) {
 		if (!is_array($images)) $images = array($images);
 		$r = self::sendRequest(array("images" => $images,"multiple" => $c),'add');
-		if ($r->status == 201 AND isset($r->project_id)) return $r->project_id;
+		if (isset($r->status) AND $r->status == 201 AND isset($r->project_id)) return $r->project_id;
 		return $r;
 	}
 
-	public static function getImage($project_id) {
+	public static function getResults($project_id) {
 		$r = self::sendRequest(array("project_id" => $project_id),'get');
-		if ($r->status == 755) return false;
+		if (isset($r->status) AND $r->status == 755) return false;
 		return $r;
+	}
+		
+	public static function getMergedResults($proj=array(),$or='domain') {
+		if (!is_array($proj)) $proj = array($proj);
+		foreach ($proj as $pr) {
+			if ($a = self::getResults($pr)) foreach ($a as $domain => $d) {
+				if ($d->pages AND !self::isFiltered($domain)) foreach ($d->pages as $entry) {
+					$entry = (array) $entry;
+					if (!self::isFiltered($domain,$entry['usage-image'])) {
+						if ($or == 'domain') $res[$domain][] = $entry;
+						else if ($or == 'image') $res[$entry['image']][] = $entry;
+					}
+				}
+			}
+		}
+		ksort($res);
+		return $res;
 	}
 	
 	public static function getCredits() {
 		$r = self::sendRequest([],'credit');
-		if ($r->status == 200) return $r->credit;
+		if (isset($r->status) AND $r->status == 200) return $r->credit;
 		return $r;
+	}
+	
+	private function isFiltered($domain,$img=false) {
+		if (!file_exists(self::FILTERSDIR)) mkdir(self::FILTERSDIR);
+		if (!file_exists(self::FILTERSDIR.'/domains-ignore.json')) file_put_contents(self::FILTERSDIR.'/domains-ignore.json','{}');
+		if (!file_exists(self::FILTERSDIR.'/not-match.json')) file_put_contents(self::FILTERSDIR.'/not-match.json','{}');
+		if (in_array($domain,(array) json_decode(file_get_contents(self::FILTERSDIR.'/domains-ignore.json')))) return true;
+		if ($img AND in_array($img,(array) json_decode(file_get_contents(self::FILTERSDIR.'/not-match.json')))) return true;
+		return false;
 	}
 	
 	private function sendRequest($data,$t,$expires=self::EXPIRES) {
@@ -41,7 +68,7 @@
 		$context = stream_context_create(array(
 			'http' => array(
 				'method' => 'POST', 
-				'header' => "Content-Type: application/json\r\nUser-Agent: IncandescentAPI PHP Class/0.1\r\n",
+				'header' => "Content-Type: application/json\r\nUser-Agent: IncandescentAPI PHP Class/0.2\r\n",
 				'content' => $json 
 			)
 		));
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-IncandescentAPI PHP Class v0.1
+IncandescentAPI PHP Class v0.2
 ==============================
 
 Simple PHP Class for IncandescentAPI
@@ -6,7 +6,13 @@ Simple PHP Class for IncandescentAPI
 Install
 -------
 
-Edit the file `IncandescentAPI.class.php` and enter your `UID` and `KEY` in first `const` fields.
+Edit the file `IncandescentAPI.class.php` and enter your `UID` and `KEY` in `const` fields. If your `UID` is _1234_ and your `KEY`is _1234567890abcdef_, your configuration must look like :
+
+	/// Configuration
+	const UID = '1234';					// Your API UID
+	const KEY = '1234567890abcdef';		// Your API KEY
+	const EXPIRES = 300;				// Default auth expiration delay
+	const FILTERSDIR = '.';			// Filters' dir without ending "/"
 
 Load it on your scripts :  
 
@@ -15,7 +21,7 @@ Load it on your scripts :
 Usage
 -----
 
-### Add Image : 
+### Add Image ::addImage() 
  
 _mixed_ `$project_id` = IncandescentAPI**::addImage** ( _mixed_ `$images` [ , _int_ `$repeat` ] )
 
@@ -25,9 +31,11 @@ Parameter `$repeat` is the number you want to search for each images (default is
 Return `$project_id` the request ID for get results or raw result if error.
 
 
-### Get Results :  
+### Get Results ::getResults()  
 
-_object_ `$results` = IncandescentAPI**::getImage** ( _string_ `$project_id` )
+_This method did not return filtered results._
+
+_object_ `$results` = IncandescentAPI**::getResults** ( _string_ `$project_id` )
 
 Parameter `$project_id` is a request ID returned by previous **::addImage()**.  
 
@@ -35,8 +43,19 @@ Return `$results` as raw IncandescentAPI results.
 Return `false` if no result.  
 See [IncandescentAPI documentation](http://incandescent.xyz)
 
+### Get Multiple Results ::getMergedResults()  
 
-### Get Credits :  
+_This method return filtered results._
+
+_object_ `$results` = IncandescentAPI**::getMergedResults** ( _mixed_ `$project_id` )
+
+Parameter `$project_id` is a request ID returned by previous **::addImage()**.  
+
+Return `$results` as raw IncandescentAPI results.  
+Return `false` if no result.  
+
+
+### Get Credits ::getCredits()
 
 _mixed_ `$credits` = IncandescentAPI**::getCredit** ( _null_ )
 
@@ -44,3 +63,14 @@ Need no parameters.
 
 Return `$credits` the numbers of credits remains or raw results if error.
 
+
+Using Filters
+-------------
+
+v0.2 introduce filters using simple JSON files :
+
+ * `not-match.json` : list all images URL that did not match on any request;  
+ * `domains-ignore.json` : list all ignored full domain (for exemple, your own domains).
+
+Directory for this files is running's script dir by default, you can set another dir with `FILTERSDIR` constant, without the ending `/`.  
+**Warning** be careful to set something or the Class will find for the filters files in root dir !
